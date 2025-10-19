646331d47e596b6100cf52a5a75f6786081b6591
RaphBag <raphbag772@gmail.com>
2025-10-19 17:56:09 +0200

    chore: remove server output configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import node from '@astrojs/node';
+// import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://raphbag.github.io",
   base: "/webtrain",
-  output: 'server',
+  // output: 'server',
   vite: {
     plugins: [tailwindcss()]
   },
-  adapter: node({
-    mode: 'standalone'
-  })
+  // adapter: node({
+  //   mode: 'standalone'
+  // })
 });
