a51e825248897447b1585bd0094567752490c0b5
RaphBag <raphbag772@gmail.com>
2025-10-19 17:47:39 +0200

    feat: add SSR for all pages with Node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://raphbag.github.io/webtrain/",
+  site: "https://raphbag.github.io",
+  base: "/webtrain",
+  output: 'server',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  adapter: node({
+    mode: 'standalone'
+  })
 });
